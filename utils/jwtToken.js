fdf3d5a9d4bbb85b2880d0878bf64900013d6fa3
Yugshah802 <shahyug.802@gmail.com>
2024-03-19 22:37:01 +0530

    first commit

---
+++ utils/jwtToken.js
@@ -0,0 +1,17 @@
+
+export const sendToken = (user, statusCode, res, message) => {
+    const token = user.getJWTToken();
+    const options = {
+      expire: new Date(
+        new Date() + process.env.COOKIE_EXPIRE 
+      ),
+      httpOnly: true, // Set httpOnly to true
+    };
+  
+    res.status(statusCode).cookie("token", token, options).json({
+      success: true,
+      user,
+      message,
+      token,
+    });
+  };
