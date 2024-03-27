63f6bd1f4c42283150117801a0fcaa4bf5494f65
Yugshah802 <shahyug.802@gmail.com>
2024-03-27 19:23:10 +0530

    sendtoken changes

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -3,11 +3,11 @@ export const sendToken = (user, statusCode, res, message) => {
     const token = user.getJWTToken();
     const options = {
       expire: new Date(
-        new Date() + process.env.COOKIE_EXPIRE 
+        Date.now() + process.env.COOKIE_EXPIRE 
       ),
       httpOnly: true, // Set httpOnly to true,
-      secure: true,
-        overwrite:true,
+      // secure: true,
+        // overwrite:true,
     };
   
     res.status(statusCode).cookie("token", token, options).json({
