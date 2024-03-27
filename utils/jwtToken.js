25a4c1c5ce88ce0e8e1acd0d8fdfd27ad5838c43
Yugshah802 <shahyug.802@gmail.com>
2024-03-27 22:27:32 +0530

    updated cookie settings

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -5,10 +5,11 @@ export const sendToken = (user, statusCode, res, message) => {
       expire: new Date(
         Date.now() + process.env.COOKIE_EXPIRE 
       ),
-      httpOnly: true, // Set httpOnly to true,
-      // secure: true,
-        // overwrite:true,
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
     };
+    console.log(token);
   
     res.status(statusCode).cookie("token", token, options).json({
       success: true,
