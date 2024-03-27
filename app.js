51a80e307110f8f28abbb7e82ec0b3be810dbc06
Yugshah802 <shahyug.802@gmail.com>
2024-03-28 01:08:10 +0530

    fix

--- app.js
+++ app.js
@@ -24,12 +24,12 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+//   next();
+// });
 
 app.set("trust proxy", 1);
   
