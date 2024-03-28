4ed1d8aa56e8940b02121605220e5fa5fe0a28d5
Yugshah802 <shahyug.802@gmail.com>
2024-03-29 00:26:13 +0530

    cors commit

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin:true,
     method: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
